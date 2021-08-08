01d4943f80b778ddd4fc57f13b74d7337ca01e93
alasacelui <alasacelui010@gmail.com>
2021-08-08 14:27:17 +0800

    Update News Module

--- src/views/News.vue
+++ src/views/News.vue
@@ -2,7 +2,7 @@
     <div class="container py-5">
       <h2 class="text-center danger display-4"> News Update <img class="pb-3" src="../../public/news.svg" alt="ph" width="60"> </h2><br>
       <div class="row justify-content-center mt-4" v-if="articles.length">
-          <div class="col-md-12" v-for="article in showArticle" :key="article.id">
+          <div class="col-md-12" v-for="article in articles" :key="article.id">
              <div class="card w-100 p-5 mb-5 shadow p-3 mb-5 bg-white rounded rounded border-light">
                  <div class="card-body">
                     <h3 class="danger font-weight-bold card-title w-75 mx-auto text-center d-lg-block d-none">{{ article.title }}</h3>
@@ -10,7 +10,7 @@
                     
                     <br>
                  </div>
-                    <img :src="article.urlToImage" v-if="article.urlToImage" ref="article_img" class="card-img-bottom loading" :title="article.title"> <br>
+                    <img :src="article.image" v-if="article.image" ref="article_img" class="card-img-bottom loading" :title="article.title"> <br>
                     <p class="card-text lead text-center">{{ article.description }}</p>
                      <div class="card-footer text-muted text-center">
                        <span>Published at : {{ $moment(article.publishedAt).format("llll") }} </span>  <br><br>
@@ -18,25 +18,7 @@
                     </div>
              </div>
           </div>
-          <nav aria-label="...">
-                <ul class="pagination pagination-md justify-content-center text-center">
-                    <li class="page-item"
-                        :class="page === 1 ? 'disabled' : ''" >
-                        
-                    <a class="page-link" @click="prevPage"> Previous </a>
-                    </li>
-                    
-                    <li class="page-link" style="background-color: inherit"> 
-                    {{ page }} of {{ lastPage }}
-                    </li>
-
-                    <li class="page-item" 
-                        :class="page === lastPage ? 'disabled' : ''" >
-                    <a class="page-link" @click="nextPage"> Next </a>
-                    </li>
-
-                </ul>
-          </nav>
+          
       </div>
       <div v-else>
           <h1>Loading ...</h1>
@@ -51,38 +33,17 @@ export default {
     data() {
         return {
             articles: [],
-            currentPage: 1,
-            page: 1,
-            perPage:20
         }
     },
     methods: {
         async getLatestNews() {
-            const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=fox-news,cnn,espn&pageSize=100&page=${this.currentPage}&apiKey=b2599a618b624fed83bfd3af155db3b5`)
+            const response = await fetch(`https://gnews.io/api/v4/top-headlines?country=ph&q=covid&token=113db9b688ed91b692a524abb418d308`)
             const {articles} = await response.json()
            
             this.articles = articles;
+            console.log(articles);
         },
-        nextPage() {
-            this.page++;
-            window.scrollTo({top: 0, behavior: 'smooth'})
-        },
-        prevPage() {
-            this.page--;
-            window.scrollTo({top: 0, behavior: 'smooth'})
-        },
-   
-    },
-    computed: {
-        showArticle () {
-            let start = (this.page - 1) * this.perPage
-            let end = start + this.perPage
-            return this.articles.slice(start, end) // 0-20
-        },
-        lastPage () {
-        let length = this.articles.length
-        return Math.ceil(length / this.perPage)
-        }
+
     },
     mounted() {
         this.getLatestNews();
